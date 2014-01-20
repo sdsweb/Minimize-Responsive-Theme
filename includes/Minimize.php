7b5bb3dd4aee03709167b95ffe8f66bf6bd0af3d
scottsousa <scott@scottjsousa.com>
2014-01-20 12:06:10 -0500

    Version 2.2.8

--- includes/Minimize.php
+++ includes/Minimize.php
@@ -3,7 +3,7 @@
  * This class manages all functionality with our Minimize v2 theme.
  */
 class Minimize {
-	const MIN_VERSION = '2.2.7';
+	const MIN_VERSION = '2.2.8';
 
 	private static $instance; // Keep track of the instance
 
